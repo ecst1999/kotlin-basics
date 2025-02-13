1610b3ac527ebc6cace0623b630e9d962b9e12ad
Steve Carvajal <javier_zam1998@live.com>
2025-02-13 15:05:22 -0500

    Colums use

--- app/src/main/java/com/ecst1999/jetpackcomposeapp/MainActivity.kt
+++ app/src/main/java/com/ecst1999/jetpackcomposeapp/MainActivity.kt
@@ -11,6 +11,7 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import com.ecst1999.jetpackcomposeapp.components.layouts.MyBox
+import com.ecst1999.jetpackcomposeapp.components.layouts.MyColumn
 import com.ecst1999.jetpackcomposeapp.login.Greeting
 import com.ecst1999.jetpackcomposeapp.ui.theme.JetPackComposeAppTheme
 
@@ -21,7 +22,7 @@ class MainActivity : ComponentActivity() {
         setContent {
             JetPackComposeAppTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    MyBox()
+                    MyColumn(Modifier.padding(innerPadding))
                 }
             }
         }
