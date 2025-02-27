9f91a1e9650cada9ded0c78e8ef33ec8b9521c62
ecst1999 <javier_zam1998@live.com>
2025-02-27 13:01:38 -0500

    States with lambda functions

--- app/src/main/java/com/ecst1999/jetpackcomposeapp/state/MyState.kt
+++ app/src/main/java/com/ecst1999/jetpackcomposeapp/state/MyState.kt
@@ -4,24 +4,38 @@ import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Column
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableIntStateOf
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 
 
 @Composable
 fun MyState(modifier: Modifier){
 
-    var number = remember { mutableStateOf(0) }
+    //var number = remember { mutableStateOf(0) }
+    //var number by rememberSaveable { mutableIntStateOf(0) }
+
+    var number by rememberSaveable { mutableIntStateOf(0) }
 
     Column {
-        Text("Pulsame: ${number.value}", modifier.clickable { number.value += 1 })
-        Text("Pulsame: ", modifier.clickable { number.value += 1 })
-        Text("Pulsame: ", modifier.clickable { number.value += 1 })
-        Text("Pulsame: ${number.value}", modifier.clickable { number.value += 1 })
-        Text("Pulsame ", modifier.clickable { number.value += 1 })
+        StateExample1(modifier, number) { number += 1 } //lambda function
+        StateExample2(modifier, number, onClick = { number += 1 })
     }
 
+}
 
+@Composable
+fun StateExample1(modifier: Modifier, number: Int, onClick: () -> Unit ){
 
+    Text("Pulsame: $number", modifier.clickable { onClick() })
+}
+
+@Composable
+fun StateExample2(modifier: Modifier, number: Int, onClick: () -> Unit){
+
+    Text("Pulsame: $number", modifier.clickable { onClick() })
 }
